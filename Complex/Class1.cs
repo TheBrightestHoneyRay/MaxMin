1d39ee834b313f8e7ec64273ae507833f461b221
Balnur <balnurquarter18@gmail.com>
2018-02-11 12:55:31 +0600

    First commit

---
+++ Complex/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex
+{
+    class Complex
+    {
+        public int x, y;
+        
+        public Complex() { }
+
+        public Complex(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+
+        }
+
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            Complex n = new Complex();
+            n.x = c1.x * c2.y + c2.x * c1.y;
+            n.y = c1.y * c2.y;
+            n.Div();
+            return n; 
+        }
+
+        public void Div()
+        {
+            int g = GCD(x, y);
+            x /= g;
+            y /= g;
+        }
+
+        private int GCD(int x, int y)
+        {
+            int res = 1;
+            for(int i = 1; i <= x; ++i)
+            {
+                if (x % i == 0 && y % i == 0)
+                    res = Math.Max(res, i);
+            }
+            return res;
+        }
+
+        public override string ToString()
+        {
+            return x + "/" + y;
+        }
+    }
+}
---
+++ Complex/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c1 = new Complex(1, 6);
+            Complex c2 = new Complex(1, 6);
+
+            Complex result = c1 + c2;
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
