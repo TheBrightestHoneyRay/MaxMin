6b88b8417014b64df0594ebeb026c3b25aabbaa1
TheBrightestHoneyRay <31367887+TheBrightestHoneyRay@users.noreply.github.com>
2018-10-07 03:32:47 +0600

    Update tdShape.java

--- Lab3/Lab3.2/src/tdShape.java
+++ Lab3/Lab3.2/src/tdShape.java
@@ -1,7 +1,59 @@
 public class tdShape {
-    public tdShape(){}
+    final static double PI = 3.14159;
+    public static abstract class Volume{
+        double h, l, r; //the height and the length and the radius
+        public abstract double getVolume();
+        public abstract double surfaceArea();
+    }
 
-    public double volume(){}
+    public static class Cube extends Volume{
+        public Cube(double a){
+            h = a;
+        }
 
-    public double surfaceArea(){}
+        @Override
+        public double getVolume(){
+            return Math.pow(h,3);
+        }
+
+        @Override
+        public double surfaceArea(){
+            return 6 * Math.pow(h,2);
+        }
+    }
+
+    public static class Sphere extends Volume{
+        public Sphere(double a){
+            r = a;
+        }
+
+        @Override
+        public double getVolume(){
+            return PI * Math.pow(r,3) * 4/3;
+        }
+
+        @Override
+        public  double surfaceArea() {
+            return 4 * PI * Math.pow(r, 2);
+
+        }
+    }
+
+    public static class Cylinder extends Volume{
+        public Cylinder(double radius, double height){
+            r = radius;
+            h = height;
+        }
+
+        @Override
+        public double getVolume(){
+            return PI*h*Math.pow(r,2);
+        }
+
+        @Override
+        public double surfaceArea(){
+            return (2*PI*Math.pow(r,2) + 2*PI*r*h);
+        }
+    }
+
 }
